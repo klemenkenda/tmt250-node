e05e8da3d814afa75b60a055ff189e7830ccf036
Klemen Kenda <klemen.kenda@ijs.si>
2019-12-30 13:33:12 +0100

    Constructor

---
+++ index.js
@@ -0,0 +1,19 @@
+class TMT250AVL {
+
+    constructor(remote, port) {
+        this.remote = remote;
+        this.port = port;
+        this.IMEI = "";
+    }
+
+    identify(buffer) {
+
+    }
+
+    decodeAVL(buffer) {
+
+    }
+
+}
+
+exports.default = TMT250AVL;
