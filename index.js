64f8b3a2761d8edb4ba4eed077c8e1111164ba37
Klemen Kenda <klemen.kenda@ijs.si>
2019-12-30 13:50:13 +0100

    Identify

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ class TMT250AVL {
     }
 
     identify(buffer) {
-        console.log(buffer);
+        const length = buffer[0] * 256 + buffer[1];
+        const IMEI = buffer.toString().substring(2);
+        if (IMEI.length === length) {
+            this.IMEI = IMEI;
+            return true;
+        }
+        return false;
     }
 
     decodeAVL(buffer) {
